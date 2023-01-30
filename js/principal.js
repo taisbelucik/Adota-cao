580c5966d2f10d4a62390f9ffa83844ef3b8b19f
taisbelucik <taisbelucik@hotmail.com>
2023-01-30 20:32:57 -0300

    primeiro commit

--- js/principal.js
+++ js/principal.js
@@ -13,28 +13,62 @@ var botaoAdicionar = document.querySelector("#adicionar-cao");
 
 botaoAdicionar.addEventListener("click", function (event) {
   event.preventDefault();
+
+  //chama a função que pega os dados do formulário
   var form = document.querySelector("#form-adiciona");
+  var cao = obtemCaoDoFormulario(form);
 
-  //pega o conteúdo dos inputs do formulário
-  var nome = form.nome.value;
-  var idade = form.idade.value;
-  var porte = form.porte.value;
-  var raça = form.raça.value;
+  //adiciona o TR e o TD dentro da tabela
+  var tabela = document.querySelector("#tabela-adocao");
 
+  //valida se está faltando informações no formulário
+  if (cao.nome === "") {
+    var faltaNome = document.querySelector("#falta-nome");
+    faltaNome.textContent = "Está faltando informar o nome";
+  }
+  if (cao.idade === "") {
+    var faltaIdade = document.querySelector("#falta-idade");
+    faltaIdade.textContent = "Está faltando informar a idade";
+  }
+  if (cao.porte === "") {
+    var faltaPorte = document.querySelector("#falta-porte");
+    faltaPorte.textContent = "Está faltando informar o porte";
+  }
+  if (cao.raça === "") {
+    var faltaRaça = document.querySelector("#falta-raça");
+    faltaRaça.textContent = "Está faltando informar a raça";
+  } else {
+    //limpa o formulário
+    form.reset();
+
+    // chama a função que cria o TR e o TD
+    var caoTr = montaTr(cao);
+
+    tabela.appendChild(caoTr);
+  }
+});
+//--------------------------------------------//
+//pega o conteúdo dos inputs do formulário
+function obtemCaoDoFormulario(form) {
+  var cao = {
+    nome: form.nome.value,
+    idade: form.idade.value,
+    porte: form.porte.value,
+    raça: form.raça.value,
+  };
+
+  return cao;
+}
+//--------------------------------------------------------//
+function montaTr(cao) {
   //cria a linha da tabela
   var caoTr = document.createElement("tr");
+  caoTr.classList.add("cao");
 
-  // cria os espaços da tabela
-  var nomeTd = document.createElement("td");
-  var idadeTd = document.createElement("td");
-  var porteTd = document.createElement("td");
-  var raçaTd = document.createElement("td");
-
-  // pega o conteúdo dos inputs do formulário e coloca nos espaços da linha
-  nomeTd.textContent = nome;
-  idadeTd.textContent = idade;
-  porteTd.textContent = porte;
-  raçaTd.textContent = raça;
+  var nomeTd = montaTd(cao.nome, "info-nome");
+  var idadeTd = montaTd(cao.idade, "info-idade");
+  var porteTd = montaTd(cao.porte, "info-porte");
+  var raçaTd = montaTd(cao.raça, "info-raça");
 
   //adiciona os espaços (td) dentro da linha(tr) com as informações
   caoTr.appendChild(nomeTd);
@@ -42,9 +76,12 @@ botaoAdicionar.addEventListener("click", function (event) {
   caoTr.appendChild(porteTd);
   caoTr.appendChild(raçaTd);
 
-  var tabela = document.querySelector("#tabela-adocao");
-
-  tabela.appendChild(caoTr);
-
-  console.log(caoTr);
-});
+  return caoTr;
+}
+//---------------------------------------------------------------//
+function montaTd(dado, classe) {
+  var td = document.createElement("td");
+  td.textContent = dado;
+  td.classList.add(classe);
+  return td;
+}
